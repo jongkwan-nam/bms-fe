fa611e397ae79df4d593e0cd058fc22833592d4b
jongkwan-nam <jongkwan.nam@gmail.com>
2023-11-02 10:51:54 +0900

    초기설정. webpack, eslint

---
+++ webpack.config.cjs
@@ -0,0 +1,78 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  /* mode: development, production, none */
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'source-map',
+  output: {
+    // filename: '[name].[contenthash].js',
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/*.html',
+          to({ context, absoluteFilename }) {
+            return '[name][ext]';
+          },
+        },
+        // {
+        //   from: 'src/css',
+        //   to: 'css',
+        // },
+        // {
+        //   from: 'src/img',
+        //   to: 'img',
+        // },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              // `postcssOptions` is needed for postcss 8.x;
+              // if you use postcss 7.x skip the key
+              postcssOptions: {
+                // postcss plugins, can be exported to postcss.config.js
+                plugins: function () {
+                  return [require('autoprefixer')];
+                },
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
